b9f63a9d34884d4042271d131d385eb4c5c4fb16
Lai-Lucky <2086287037@qq.com>
2025-07-02 11:10:35 +0800

    v2.0:接入数据终端

--- src/main.cpp
+++ src/main.cpp
@@ -7,33 +7,20 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
+#include <freertos/FreeRTOS.h> // 添加 FreeRTOS 头文件
+#include <freertos/task.h>     // 添加 FreeRTOS 任务管理头文件
+#include <freertos/semphr.h>   // 添加 FreeRTOS 信号量头文件
+
 
 /************** 函数声明 ***************/
 uint16_t CRC16(const uint8_t *data, uint16_t length);
 bool checkCRC(const uint8_t *data, uint16_t len);
 void parseModbusData(const uint8_t *data, uint16_t len);
-void setup_wifi();
-void callback(char* topic, byte* payload, unsigned int length);
-void reconnect();
 void sendSensorData(double data) ;
 
-/************** WiFi 配置 **************/
-const char* ssid = "abc";         // WiFi SSID
-const char* password = "12345678"; // WiFi 密码
+void soilsensor_task_vtask(void *pv); //土壤传感器任务
 
-/************ OneNet MQTT 配置 ************/
-const char* mqtt_server = "mqtts.heclouds.com";  
-const int mqtt_port = 1883; 
-const char* device_id = "test-v1";    
-const char* product_id = "9x4t8eHzq0"; 
-const char* api_key = "version=2018-10-31&res=products%2F9x4t8eHzq0%2Fdevices%2Ftest-v1&et=999986799814791288&method=md5&sign=OVAcjwaY2uLe09tz04ZfSA%3D%3D";
-
-// **MQTT 主题**
-const char* pubTopic = "$sys/9x4t8eHzq0/test-v1/thing/property/post";
-const char* replyTopic="$sys/9x4t8eHzq0/test-v1/thing/property/post/reply";
-
-WiFiClient espClient;
-PubSubClient client(espClient);
+TaskHandle_t soilsensor_task_handle;  //土壤传感器任务句柄
 
 /************* 询问命令 *************/
 const byte send_byte[1][8] = {
@@ -52,45 +39,13 @@ void setup() {
   Serial.begin(9600);
   Serial2.begin(9600);
 
-  setup_wifi();
-  client.setServer(mqtt_server, mqtt_port);
-  client.setCallback(callback);
+  xTaskCreate(soilsensor_task_vtask,"soilsensor_task_vtask",4096,NULL,1,&soilsensor_task_handle);//创建土壤传感器任务
+  
 }
 
 /************* 主循环 *************/
 void loop() {
-  if (!client.connected()) 
-  {
-    reconnect();
-  }
-  client.loop();
-
-  // 发送请求
-  Serial2.write(send_byte[asr], 8);
-  Serial.printf("发送查询: %s\n", sensor_names[asr]);
-  Serial.println();
-
-  delay(200);
-
-  // 读取响应数据
-  if (Serial2.available()) 
-  {
-    int len = Serial2.available();
-    Serial2.readBytes(temp, len);
-    Serial.println("接收传感器数据");
-    parseModbusData(temp, len);
-
-    if (checkCRC(temp, len)) 
-    {
-      Serial.println("CRC 校验成功\n");
-    } 
-    else 
-    {
-      Serial.println("CRC 校验失败\n");
-    }
-  }
-
-  delay(1000);
+  
 }
 
 /************* CRC 计算 *************/
@@ -127,76 +82,17 @@ void parseModbusData(const uint8_t *data, uint16_t len) {
     {  // 7字节
       uint16_t regValue = (data[3] << 8) | data[4];
       int32_t value = regValue ;
-      Serial.printf("解析数据: %d\n", value);
       sendSensorData(value);
     } 
   }
 }
 
-/************* WiFi 连接 *************/
-void setup_wifi() {
-  Serial.println("连接 WiFi...");
-  WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) 
-  {
-    delay(1000);
-    Serial.print(".");
-  }
-
-  Serial.println("\nWiFi 连接成功!");
-  Serial.print("IP 地址: ");
-  Serial.println(WiFi.localIP());
-}
-
-/************* MQTT 订阅回调函数 *************/
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("收到 MQTT 消息，主题: \n");
-  Serial.println(topic);
-  Serial.print("内容: ");
-  for (int i = 0; i < length; i++) 
-  {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-  Serial.println();
-}
-
-/************* 连接 MQTT 服务器 *************/
-void reconnect() {
-  while (!client.connected()) 
-  {
-    Serial.print("连接 OneNet MQTT...");
-
-    if (client.connect(device_id, product_id, api_key)) 
-    {
-      Serial.println("连接成功!");
-      client.subscribe(replyTopic); // 订阅属性下发
-    } 
-    else 
-    {
-      Serial.printf("连接失败, 状态码=%d, 5秒后重试...\n", client.state());
-      switch (client.state()) 
-      {
-        case -4: Serial.println("连接超时"); break;
-        case -3: Serial.println("连接丢失"); break;
-        case -2: Serial.println("连接失败"); break;
-        case -1: Serial.println("断开连接"); break;
-        case 1: Serial.println("协议错误"); break;
-        case 2: Serial.println("客户端标识无效"); break;
-        case 3: Serial.println("服务器不可用"); break;
-        case 4: Serial.println("用户名或密码错误"); break;
-        case 5: Serial.println("未授权"); break;
-        default: Serial.println("未知错误");
-      }
-      delay(5000);
-    }
-  }
-}
 
 /************JSON数据构建************/
 void sendSensorData(double data) 
 {
+  vTaskDelay(pdMS_TO_TICKS(100));
   JsonDocument doc;
   doc["id"] = String(millis());  // 使用时间戳作为唯一ID
   doc["version"] = "1.0";
@@ -204,14 +100,31 @@ void sendSensorData(double data)
 
   String payload;
   serializeJson(doc, payload);
-  if (client.publish(pubTopic, payload.c_str())) 
-  {
-    Serial.println("数据已发送: " + payload);
-  } 
-  else 
-  {
-    Serial.println("发送失败");
-  }
 
-  delay(200);
+  Serial.println(payload.c_str());
+
+  vTaskDelay(pdMS_TO_TICKS(100));
+
+}
+
+/********************* 土壤传感器任务 *********************/
+void soilsensor_task_vtask(void *pv){
+
+  while(1)
+  {
+    // 发送请求
+    Serial2.write(send_byte[asr], 8);
+
+    vTaskDelay(pdMS_TO_TICKS(200));
+
+    // 读取响应数据
+    if (Serial2.available()) 
+    {
+      int len = Serial2.available();
+      Serial2.readBytes(temp, len);
+      parseModbusData(temp, len);
+      
+      vTaskDelay(pdMS_TO_TICKS(500));
+    }
+  }
 }
